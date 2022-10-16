d65e268590dd0aa0daac9e86081972559cd538dd
Rina <rinakino73@gmail.com>
2022-10-16 20:10:40 +0900

    cleaned

--- catenarysim.py
+++ catenarysim.py
@@ -19,7 +19,7 @@ nozzleDiameter = 0.4
 
 ### output
 #Catenary = DataTree[object]()
-catenary =[]
+cat =[]
 debugger =[]
 
 ## Catenary direction
@@ -34,6 +34,7 @@ else:
 ##### CLASS #####
 #################
 
+# A class for keeping track of each Catenaries
 class Catenary:
 
     def __init__(self, aPt, bPt, len, dir):
@@ -49,10 +50,9 @@ class Catenary:
         self.extrdSrf = self.extrd()
     
     def draw(self):
-        print("Catenary.draw")
         cat = gh.Catenary(self.aPt, self.bPt, self.len, self.dir)
 
-        ## check catenary wrong gravity and 
+        ## check cat wrong gravity direction and correct
         ## compare line and cat
         line = rs.AddLine(self.aPt, self.bPt)
         lineMidPntZ = rs.CurveMidPoint(line)[2]
@@ -63,14 +63,12 @@ class Catenary:
         else:
             if catMidPntZ > lineMidPntZ:
                 cat = gh.Catenary(self.aPt, self.bPt, self.len, self.dir*-1)
-
         
         ## flip Curve Direction
         if not rs.CurveDirectionsMatch(cat, rs.AddLine(self.aPt, self.bPt)):
-            print("reverse")
             cat.Reverse()
         
-        ## unitize curve domain
+        ## unitize curve domain to 0-1
         cat.Domain = rc.Geometry.Interval(0,1)
         
         return cat
@@ -82,7 +80,8 @@ class Catenary:
         return rs.ExtrudeCurve(self.cat, path)
         
 
-
+# a class for keeping track of each chain (small catenaries chained together)
+# methods for generating tangled chains as Catenary class objects
 class Chain:
 
     def __init__(self, aPt, bPt, len, dir):
@@ -91,146 +90,110 @@ class Chain:
         self.len = len
         self.dir = dir
         
-        ## Catenary class instance
-        ## before tangle
-        self.initCat = Catenary(self.aPt, self.bPt, self.len, self.dir)
-
-        ## a list of Catenary class instances
-        ## after tangle
-        self.catenaries = []
+        self.catBeforeTangle = Catenary(self.aPt, self.bPt, self.len, self.dir)
+        self.tangledCat_list = []
 
 
-    def tangle(self, prevChainCat_list):
+    def tangle(self, prevChain_list):
         print("Chain.tangle")
-
-        ## if first Chain, no obstacle
-        if prevChainCat_list == []:
-            
-            print("first chain")
-
-            ## append first Chain as Catenary class
-            self.catenaries.append(self.initCat)
-            return self.catenaries
-
-        ## after first Chain, check collision
-        else:
-            bendingPnt_list = []
-            bendingPnt_list.append(self.aPt)
-            bendingPnt_list.append(self.bPt)
+        
+        ## make a list of tangling points
+        def tanglingPnts():
+            ## append first and last point/params
+            tanglingPnt_list = []
+            tanglingPnt_list.append(self.aPt)
+            tanglingPnt_list.append(self.bPt)
 
             collisionPntParam_list = []
             collisionPntParam_list.append(0)
             collisionPntParam_list.append(1)
 
-            for prevChainCats in prevChainCat_list:
-                for prevCat in prevChainCats:
+            ## check collision if there are previous Chains
+            if prevChain_list != []:
+                for prevChain in prevChain_list:
+                    for prevCat in prevChain.tangledCat_list:
+                        # find if this chain collides with obstacles ( areas below previous chains )
 
-                    print(prevCat)
-                    ## check if this chain collides with obstacles ( previous chains )
-                    print(self.initCat.cat, prevCat.extrdSrf)
-                    if not prevCat.extrdSrf:
-                        break
-                    collisionCheck = rs.CurveSurfaceIntersection(self.initCat.cat, prevCat.extrdSrf)
-                    print("collisionCheck")
-                    
-                    if collisionCheck:
-                        if collisionCheck[0][0] == 1:
-                            print("collisionCheck True")
-                            #print(collisionCheck[0][1])
+                        collisionCheck = rs.CurveSurfaceIntersection(self.catBeforeTangle.cat, prevCat.extrdSrf)
+                        
+                        if collisionCheck:
+                            if collisionCheck[0][0] == 1:
+                                # find and append tangling point
+                                                                                                                                
+                                ## pnt and param where chain collides with obstacles 
+                                collisionPnt = collisionCheck[0][1]
+                                collisionPntParm = collisionCheck[0][5]
+                                
+                                ## find tangling points
+                                movePnt = rs.CopyObject(collisionPnt, gravity * -1 * maxLen)
+                                projectLine = rs.AddLine(collisionPnt, movePnt)                           
+                                tangling = rs.CurveCurveIntersection(projectLine, prevCat.cat)                            
+                                tanglingPnt = tangling[0][1]
 
-                            ## append undivided Chain as Catenary class                                                                        
-                            """ EDIT """
-                            #self.catenaries.append(self.initCat)
-                            ## pnt and param where chain collides with obstacles 
-                            collisionPnt = collisionCheck[0][1]
-                            collisionPntParm = collisionCheck[0][5]
+                                ## move tangling points up
+                                ## to express the thickness of the chain
+                                rs.MoveObject(tanglingPnt, gravity * -1 * nozzleDiameter)
 
-                            movePnt = rs.CopyObject(collisionPnt, gravity * -1 * maxLen)
-                            projectLine = rs.AddLine(collisionPnt, movePnt)
-
-                            ## get colliding points
-                            projectLine = rs.coercecurve(projectLine)                            
-                            bending = rs.CurveCurveIntersection(projectLine, prevCat.cat)                            
-                            bendingPnt = bending[0][1]
-
-                            print("bendingPnt")
-                            print(bendingPnt)
-
-                            ## move colliding points up
-                            rs.MoveObject(bendingPnt, gravity * -1 * nozzleDiameter)
-
-                            bendingPnt_list.append(bendingPnt)
-                            collisionPntParam_list.append(collisionPntParm)                   
-                            
-                    else:
-                        print("collisionCheck False")
-
-                        ## append undivided Chain as Catenary class
-                        #self.catenaries.append(self.initCat)
-            
+                                ## append tangling points and param that represents the orderr of points
+                                tanglingPnt_list.append(tanglingPnt)
+                                collisionPntParam_list.append(collisionPntParm)                   
+                                
             ## reorder pnts
-            print(collisionPntParam_list)
-            bendingPnt_listSorter = zip(collisionPntParam_list, bendingPnt_list)
-            bendingPnt_listSorter.sort()
-            collisionPntParam_list, bendingPnt_list = zip(* bendingPnt_listSorter)
-            print(collisionPntParam_list)
+            tanglingPnt_listSorter = zip(collisionPntParam_list, tanglingPnt_list)
+            tanglingPnt_listSorter.sort()
+            collisionPntParam_list, tanglingPnt_list = zip(* tanglingPnt_listSorter)
 
             ## Cull duplicate pnts
-            bendingPnt_list = rs.CullDuplicatePoints(bendingPnt_list)
+            tanglingPnt_list = rs.CullDuplicatePoints(tanglingPnt_list)
 
+            return tanglingPnt_list
+        
 
-            for i in range( len(bendingPnt_list) -1):
-                aPt = bendingPnt_list[i]
-                bPt = bendingPnt_list[i+1]
+        # make a list of tangled catenaries
+        def tanglingChains( tanglingPnt_list ):
+            for i in range( len(tanglingPnt_list) -1):
+                aPt = tanglingPnt_list[i]
+                bPt = tanglingPnt_list[i+1]
                 dist = rs.Distance(aPt, bPt)
                 catLen = dist ## change
 
+                cat = Catenary(aPt, bPt, catLen, self.dir)
 
-                catenary = Catenary(aPt, bPt, catLen, self.dir)
-                self.catenaries.append(catenary)
+                self.tangledCat_list.append(cat)
 
             ### get divided length
-            for i in range( len(self.catenaries) ):
-                cat = self.catenaries[i]
+            for i in range( len(self.tangledCat_list) ):
+                cat = self.tangledCat_list[i]
                 if rs.Distance(cat.aPt, cat.bPt) < nozzleDiameter*2:
                     cat.len *= 1
                 else:
-                    cat.len *= 2
+                    cat.len *= 1.1
                     cat.rerun()
 
-
-
-
-
+            return self.tangledCat_list
             
-
-
-
-                        
+        
+        return tanglingChains( tanglingPnts() )
 
 #####################
 ##### CLASS END #####
 #####################
 
 
-# tangledChainCat_list -> a list of tangled Catenary class instances
-
-# chain -> Chain class instance
-
-tangledChainCat_list = []
-
+# tangledChain_list -> a list of tangled Chain class instances
+tangledChain_list = []
 
 for lineN, line in enumerate( lines ):
     line = rs.coercecurve( line )
     chain = Chain( rs.CurveStartPoint(line),rs.CurveEndPoint(line),y[lineN],gravity )
-    chain.tangle( tangledChainCat_list )
-    tangledChainCat_list.append( chain.catenaries )
+    chain.tangle( tangledChain_list )
+    tangledChain_list.append( chain )
 
 
 print("result")
 
 catenary =[]
-print(tangledChainCat_list)
-for tangledChainCat in tangledChainCat_list:
-    for cat in tangledChainCat:
-        catenary.append(cat.cat)
+print(tangledChain_list)
+for tangledChain in tangledChain_list:
+    for tangledCat in tangledChain.tangledCat_list:
+        catenary.append(tangledCat.cat)
